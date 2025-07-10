d1e676818f8db20391523eae5ead82153b24b6d8
iamHari01 <gowribalan95@gmail.com>
2025-07-10 14:04:07 +0530

    Zoho prep

---
+++ LinkedList/src/LinkedList.java
@@ -0,0 +1,29 @@
+
+
+public class LL {
+    Node head;
+    class Node {
+        int data;
+        Node next;
+        Node(int val){
+            data = val;
+            next = null;
+
+        }
+    }
+    LL(){
+        head = null;
+
+    }
+    public void insertAtBeginning(int val){
+        Node newNode = new Node(val);
+
+
+    }
+
+
+
+
+
+
+}
